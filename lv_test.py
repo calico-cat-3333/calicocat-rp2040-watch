5c234b142511fbb3cf54231764553f5c482f55c6
Calico cat <ahsdhmzyn@163.com>
2025-01-24 10:59:26 +0800

    update display driver, remove pure python display driver

--- lv_test.py
+++ lv_test.py
@@ -1,42 +1,15 @@
 import lvgl as lv
 import lv_utils
 import board
-import tft_config
+from tft_config import GC9A01_lv
 from cst816s_lv import CST816S_lv
 import time
 import gc
 
-
-tft = tft_config.config(0)
-
-color_format = lv.COLOR_FORMAT.RGB565
-pixel_size = lv.color_format_get_size(color_format)
-factor = 4
-width = 240
-height = 240
-
-def ddflush_cb(disp_drv, area, color_p):
-    w = area.x2 - area.x1 + 1
-    h = area.y2 - area.y1 + 1
-    size = w * h
-    data_view = color_p.__dereference__(size * pixel_size)
-    lv.draw_sw_rgb565_swap(data_view, size)
-    tft.blit_buffer(data_view, area.x1, area.y1, w, h)
-    disp_drv.flush_ready()
-
 if not lv.is_initialized(): lv.init()
 if not lv_utils.event_loop.is_running(): event_loop=lv_utils.event_loop()
 
-draw_buf1 = lv.draw_buf_create(width, height // factor, color_format, 0)
-draw_buf2 = lv.draw_buf_create(width, height // factor, color_format, 0)
-#draw_buf2 = None
-
-disp_drv = lv.display_create(width, height)
-disp_drv.set_color_format(color_format)
-disp_drv.set_draw_buffers(draw_buf1, draw_buf2)
-disp_drv.set_render_mode(lv.DISPLAY_RENDER_MODE.PARTIAL)
-disp_drv.set_flush_cb(ddflush_cb)
-
+disp_drv = GC9A01_lv(board.lcd_spi, board.lcd_rst, board.lcd_cs, board.lcd_dc, board.lcd_bl)
 tp_drv = CST816S_lv(board.i2c1, board.tp_int, board.tp_rst)
 
 scr = lv.obj()
--- tft_config.py
+++ tft_config.py
@@ -1,30 +1,59 @@
 """RP2040-Touch-LCD-1.28
 """
 
-from machine import Pin, SPI
+import board
 import gc9a01
+import lvgl as lv
 
-TFA = 0
-BFA = 0
-WIDE = 0
-TALL = 1
+class GC9A01_lv:
+    def __init__(self, spi, rst, cs, dc, bl, doublebuffer=True):
+        if not lv.is_initialized():
+            lv.init()
 
+        self.width = 240
+        self.height = 240
+        self.color_format = lv.COLOR_FORMAT.RGB565
+        self.pixel_size = lv.color_format_get_size(self.color_format)
+        self.factor = 4
 
-def config(rotation=0, buffer_size=0, options=0):
-    """Configure the display and return an instance of gc9a01.GC9A01."""
+        self.tft_drv = gc9a01.GC9A01(
+            spi,
+            self.width,
+            self.height,
+            reset=rst,
+            cs=cs,
+            dc=dc,
+            rotation=0,
+            options=0,
+            buffer_size=0
+        )
+        self.tft_drv.init()
 
-    spi = SPI(1, baudrate=60000000, sck=Pin(10), mosi=Pin(11))
-    scr = gc9a01.GC9A01(
-        spi,
-        240,
-        240,
-        reset=Pin(13, Pin.OUT),
-        cs=Pin(9, Pin.OUT),
-        dc=Pin(8, Pin.OUT),
-        backlight=Pin(25, Pin.OUT),
-        rotation=rotation,
-        options=options,
-        buffer_size=buffer_size
-    )
-    scr.init()
-    return scr
+        self.backlight = bl
+        if self.backlight is not None:
+            self.backlight.value(1)
+
+        self.draw_buf1 = lv.draw_buf_create(self.width, self.height // self.factor, self.color_format, 0)
+        self.draw_buf2 = lv.draw_buf_create(self.width, self.height // self.factor, self.color_format, 0) if doublebuffer else None
+
+        self.disp_drv = lv.display_create(self.width, self.height)
+        self.disp_drv.set_color_format(self.color_format)
+        self.disp_drv.set_draw_buffers(self.draw_buf1, self.draw_buf2)
+        self.disp_drv.set_render_mode(lv.DISPLAY_RENDER_MODE.PARTIAL)
+        self.disp_drv.set_flush_cb(self.tft_flush_cb)
+
+    def tft_flush_cb(self, disp_drv, area, color_p):
+        w = area.x2 - area.x1 + 1
+        h = area.y2 - area.y1 + 1
+        size = w * h
+        data_view = color_p.__dereference__(size * self.pixel_size)
+        lv.draw_sw_rgb565_swap(data_view, size)
+        self.tft_drv.blit_buffer(data_view, area.x1, area.y1, w, h)
+        disp_drv.flush_ready()
+
+    def set_backlight_value(self, brightness):
+        if brightness > 100:
+            brightness = 100
+        if brightness < 10:
+            brightness = 10
+
