044c246e67437c05267171babb495501731f8bff
Calico cat <ahsdhmzyn@163.com>
2025-01-25 00:29:14 +0800

    add const to cst816s.py

--- cst816s.py
+++ cst816s.py
@@ -1,58 +1,59 @@
 import time
+from micropython import const
 
-_CST816_Address = 0x15
+_CST816_Address = const(0x15)
 
 # CST816S_Register
-_CST816_GestureID = 0x01
-_CST816_FingerNum = 0x02
-_CST816_XposH = 0x03
-_CST816_XposL = 0x04
-_CST816_YposH = 0x05
-_CST816_YposL = 0x06
+_CST816_GestureID = const(0x01)
+_CST816_FingerNum = const(0x02)
+_CST816_XposH = const(0x03)
+_CST816_XposL = const(0x04)
+_CST816_YposH = const(0x05)
+_CST816_YposL = const(0x06)
 
-_CST816_ChipID = 0xA7
-_CST816_ProjID = 0xA8
-_CST816_FwVersion = 0xA9
-_CST816_MotionMask = 0xAA
+_CST816_ChipID = const(0xA7)
+_CST816_ProjID = const(0xA8)
+_CST816_FwVersion = const(0xA9)
+_CST816_MotionMask = const(0xAA)
 
-_CST816_BPC0H = 0xB0
-_CST816_BPC0L = 0xB1
-_CST816_BPC1H = 0xB2
-_CST816_BPC1L = 0xB3
+_CST816_BPC0H = const(0xB0)
+_CST816_BPC0L = const(0xB1)
+_CST816_BPC1H = const(0xB2)
+_CST816_BPC1L = const(0xB3)
 
-_CST816_IrqPluseWidth = 0xED
-_CST816_NorScanPer = 0xEE
-_CST816_MotionSlAngle = 0xEF
-_CST816_LpScanRaw1H = 0XF0
-_CST816_LpScanRaw1L = 0XF1
-_CST816_LpScanRaw2H = 0XF2
-_CST816_LpScanRaw2L = 0XF3
-_CST816_LpAutoWakeTime = 0XF4
-_CST816_LpScanTH = 0XF5
-_CST816_LpScanWin = 0XF6
-_CST816_LpScanFreq = 0XF7
-_CST816_LpScanIdac = 0XF8
-_CST816_AutoSleepTime = 0XF9
-_CST816_IrqCtl = 0XFA
-_CST816_AutoReset = 0XFB
-_CST816_LongPressTime = 0XFC
-_CST816_IOCtl = 0XFD
-_CST816_DisAutoSleep = 0XFE
+_CST816_IrqPluseWidth = const(0xED)
+_CST816_NorScanPer = const(0xEE)
+_CST816_MotionSlAngle = const(0xEF)
+_CST816_LpScanRaw1H = const(0XF0)
+_CST816_LpScanRaw1L = const(0XF1)
+_CST816_LpScanRaw2H = const(0XF2)
+_CST816_LpScanRaw2L = const(0XF3)
+_CST816_LpAutoWakeTime = const(0XF4)
+_CST816_LpScanTH = const(0XF5)
+_CST816_LpScanWin = const(0XF6)
+_CST816_LpScanFreq = const(0XF7)
+_CST816_LpScanIdac = const(0XF8)
+_CST816_AutoSleepTime = const(0XF9)
+_CST816_IrqCtl = const(0XFA)
+_CST816_AutoReset = const(0XFB)
+_CST816_LongPressTime = const(0XFC)
+_CST816_IOCtl = const(0XFD)
+_CST816_DisAutoSleep = const(0XFE)
 
 # CST816S_Mode
-_CST816S_Point_Mode = 1
-_CST816S_Gesture_Mode = 2
-_CST816S_ALL_Mode = 3
+_CST816S_Point_Mode = const(1)
+_CST816S_Gesture_Mode = const(2)
+_CST816S_ALL_Mode = const(3)
 
 # CST816S_Gesture
-_CST816S_Gesture_None = 0
-_CST816S_Gesture_Up = 1
-_CST816S_Gesture_Down = 2
-_CST816S_Gesture_Left = 3
-_CST816S_Gesture_Right = 4
-_CST816S_Gesture_Click = 5
-_CST816S_Gesture_Double_Click = 0x0B
-_CST816S_Gesture_Long_Press=0x0C
+_CST816S_Gesture_None = const(0)
+_CST816S_Gesture_Up = const(1)
+_CST816S_Gesture_Down = const(2)
+_CST816S_Gesture_Left = const(3)
+_CST816S_Gesture_Right = const(4)
+_CST816S_Gesture_Click = const(5)
+_CST816S_Gesture_Double_Click = const(0x0B)
+_CST816S_Gesture_Long_Press = const(0x0C)
 
 class CST816S:
     def __init__(self, i2c, int_pin, rst_pin, *, address=_CST816_Address, sleep=True):
