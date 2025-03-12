1b3adcfda8fcec9610fac09b9719ef4654e9258a
Calico cat <ahsdhmzyn@163.com>
2025-03-12 22:16:26 +0800

    update system.py

--- cces/system.py
+++ cces/system.py
@@ -7,34 +7,33 @@ from . import hal
 from . import task_scheduler
 from . import daily_scheduler
 
-class CCES:
-    def __init__(self):
-        pass
 
-    def load_lvgl(self):
-        lv.init()
-        self.lv_event_loop = lv_utils.event_loop()
-        self.fs_drv = lv.fs_drv_t()
-        fs_driver.fs_register(fs_drv, 'S')
-        hal.after_lvgl_init()
+def load_lvgl():
+    global lv_event_loop
+    global fs_driver
+    lv.init()
+    lv_event_loop = lv_utils.event_loop()
+    fs_drv = lv.fs_drv_t()
+    fs_driver.fs_register(fs_drv, 'S')
+    hal.after_lvgl_init()
 
-    def load_system_service(self):
-        daily_scheduler.start()
+def load_system_service():
+    daily_scheduler.start()
 
-    def load_apps(self):
-        # todo: load apps
-        pass
+def load_apps():
+    # todo: load apps
+    pass
 
-    def start(self):
-        self.load_lvgl()
-        self.load_system_service()
-        self.load_apps()
+def start():
+    load_lvgl()
+    load_system_service()
+    load_apps()
 
-        # todo: start watchface activity
+    # todo: start watchface activity
 
-        while True:
-            task = task_scheduler.get_due_task()
-            if task != None:
-                task()
-            else:
-                time.sleep_ms(1)
+    while True:
+        task = task_scheduler.get_due_task()
+        if task != None:
+            task()
+        else:
+            time.sleep_ms(1)
