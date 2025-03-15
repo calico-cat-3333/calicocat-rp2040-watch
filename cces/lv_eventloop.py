3076db8b014e77cf19b2078d0f7d88c597df1a3d
Calico cat <ahsdhmzyn@163.com>
2025-03-15 22:27:43 +0800

    lv.tick_inc 现在独立计时

--- cces/lv_eventloop.py
+++ cces/lv_eventloop.py
@@ -1,23 +1,24 @@
 from .task_scheduler import Task
+from machine import Timer
+from micropython import const
 
 import time
 import lvgl as lv
 
-def start():
-    lv_eventloop = Task(lv_eventloop_task, 40)
-    lv_eventloop.start()
-    return lv_eventloop
+period = const(40)
 
-last_tick = time.ticks_ms()
+def start():
+    global lv_eventloop
+    global lv_timer
+    lv_timer = Timer(-1)
+    lv_timer.init(mode=Timer.PERIODIC, period=period, callback=lv_eventloop_timer)
+    lv_eventloop = Task(lv_eventloop_task, period)
+    lv_eventloop.start()
+    return (lv_eventloop, lv_timer)
 
 def lv_eventloop_task():
-    global last_tick
-    current_tick = time.ticks_ms()
-    timeuse = time.ticks_diff(current_tick, last_tick)
-    lv.tick_inc(timeuse)
-    last_tick = current_tick
     if lv._nesting.value == 0:
-        try:
-            lv.task_handler()
-        except Exception as e:
-            print(e)
+        lv.task_handler()
+
+def lv_eventloop_timer():
+    lv.tick_inc(period)
