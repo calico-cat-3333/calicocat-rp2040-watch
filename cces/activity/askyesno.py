2eb31833402217185cb9daf04692f1267465601c
Calico cat <ahsdhmzyn@163.com>
2025-03-17 19:36:41 +0800

    update info and yesno activity style

--- cces/activity/askyesno.py
+++ cces/activity/askyesno.py
@@ -13,26 +13,35 @@ class AskYesNoActivity(Activity):
 
     def setup(self):
         self.title_label = lv.label(self.scr)
-        self.title_label.align(lv.ALIGN.TOP_MID, 0, 40)
-        self.title_label.set_style_text_font(lv.font_montserrat_24, 0)
+        self.title_label.align(lv.ALIGN.TOP_MID, 0, 30)
+        self.title_label.set_width(160)
+        self.title_label.set_long_mode(lv.label.LONG_MODE.SCROLL_CIRCULAR)
+        self.title_label.set_style_text_font(lv.font_montserrat_24, lv.PART.MAIN | lv.STATE.DEFAULT)
         self.title_label.set_text(self.title)
 
         self.text_label = lv.label(self.scr)
-        self.text_label.align(lv.ALIGN.CENTER, 0, -30)
+        self.text_label.align(lv.ALIGN.CENTER, 0, 0)
+        self.text_label.set_width(200)
         self.text_label.set_text(self.text)
 
         self.yes_btn = lv.button(self.scr)
-        self.yes_btn.align(lv.ALIGN.BOTTOM_MID, 0, -65)
+        self.yes_btn.align(lv.ALIGN.BOTTOM_LEFT, 0, 0)
+        self.yes_btn.set_style_radius(0, lv.PART.MAIN | lv.STATE.DEFAULT)
+        self.yes_btn.set_size(119, 60)
         self.yes_btn.add_event_cb(self.yes_btn_click_cb, lv.EVENT.CLICKED, None)
         self.yes_btn_label = lv.label(self.yes_btn)
-        self.yes_btn_label.set_style_text_font(lv.font_montserrat_24, 0)
+        self.yes_btn_label.align(lv.ALIGN.RIGHT_MID, 0, 0)
+        self.yes_btn_label.set_style_text_font(lv.font_montserrat_24, lv.PART.MAIN | lv.STATE.DEFAULT)
         self.yes_btn_label.set_text(self.yes_label_text)
 
         self.no_btn = lv.button(self.scr)
-        self.no_btn.align(lv.ALIGN.BOTTOM_MID, 0, -15)
+        self.no_btn.align(lv.ALIGN.BOTTOM_RIGHT, 0, 0)
+        self.no_btn.set_style_radius(0, lv.PART.MAIN | lv.STATE.DEFAULT)
+        self.no_btn.set_size(119, 60)
         self.no_btn.add_event_cb(self.no_btn_click_cb, lv.EVENT.CLICKED, None)
         self.no_btn_label = lv.label(self.no_btn)
-        self.no_btn_label.set_style_text_font(lv.font_montserrat_24, 0)
+        self.no_btn_label.align(lv.ALIGN.LEFT_MID, 0, 0)
+        self.no_btn_label.set_style_text_font(lv.font_montserrat_24, lv.PART.MAIN | lv.STATE.DEFAULT)
         self.no_btn_label.set_text(self.no_label_text)
 
 
--- cces/activity/info.py
+++ cces/activity/info.py
@@ -11,19 +11,24 @@ class InfoActivity(Activity):
 
     def setup(self):
         self.title_label = lv.label(self.scr)
-        self.title_label.align(lv.ALIGN.TOP_MID, 0, 40)
-        self.title_label.set_style_text_font(lv.font_montserrat_24, 0)
+        self.title_label.align(lv.ALIGN.TOP_MID, 0, 30)
+        self.title_label.set_width(160)
+        self.title_label.set_long_mode(lv.label.LONG_MODE.SCROLL_CIRCULAR)
+        self.title_label.set_style_text_font(lv.font_montserrat_24, lv.PART.MAIN | lv.STATE.DEFAULT)
         self.title_label.set_text(self.title)
 
         self.text_label = lv.label(self.scr)
-        self.text_label.align(lv.ALIGN.CENTER, 0, -20)
+        self.text_label.align(lv.ALIGN.CENTER, 0, 0)
+        self.text_label.set_width(200)
         self.text_label.set_text(self.text)
 
         self.ok_btn = lv.button(self.scr)
-        self.ok_btn.align(lv.ALIGN.BOTTOM_MID, 0, -15)
+        self.ok_btn.align(lv.ALIGN.BOTTOM_MID, 0, 0)
+        self.ok_btn.set_size(240, 60)
         self.ok_btn.add_event_cb(self.ok_btn_click_cb, lv.EVENT.CLICKED, None)
         self.ok_btn_label = lv.label(self.ok_btn)
-        self.ok_btn_label.set_style_text_font(lv.font_montserrat_24, 0)
+        self.ok_btn_label.align(lv.ALIGN.CENTER, 0, 0)
+        self.ok_btn_label.set_style_text_font(lv.font_montserrat_24, lv.PART.MAIN | lv.STATE.DEFAULT)
         self.ok_btn_label.set_text(self.ok_label_text)
 
     def ok_btn_click_cb(self, event):
