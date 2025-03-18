8bfceadaecd357aea3490b059ddc0db137ffdbea
Calico cat <ahsdhmzyn@163.com>
2025-03-18 23:54:48 +0800

    fix error in ble driver

--- cces/drvs/e104ble.py
+++ cces/drvs/e104ble.py
@@ -3,13 +3,18 @@ from pcf8574 import PCF8574
 import micropython
 
 from . import Device
-from .. import log
+from ..log import log, ERROR
+
+# E104-BT5005A 蓝牙串口模块驱动
+# 此驱动被设计为只能被一个服务读写
+# 虽然没有任何措施保证
 
 class E104BLE(Device):
     def __init__(self, rx, tx, pcf8574_i2c):
         self.rx_pin = rx
         self.tx_pin = tx
         self.uart = UART(0, rx=rx, tx=tx, baudrate=115200)
+        self.uart_rx_read_to_buf_ref = self.uart_rx_read_to_buf
         self.uart.irq(handler=self.uart_rx_int_cb, trigger=UART.IRQ_RXIDLE)
         self.rx_line_buf = []
         self.rx_buf = ''
@@ -19,8 +24,6 @@ class E104BLE(Device):
         self.rst_pin = self.pcf8574.get_pin(5)
         self.wkp_pin = self.pcf8574.get_pin(3)
         self.wkp_pin.value(0)
-
-        self uart_rx_read_to_buf_ref = self.uart_rx_read_to_buf
 
     def at_mode(v=None):
         if v:
@@ -36,11 +39,14 @@ class E104BLE(Device):
             log('uart rx receive:', r)
             self.rx_buf = self.rx_buf + r.decode()
             if self.rx_buf.endswith('\n'):
+                if self.uart_rx_any() > 10:
+                    log('too may rx message in rx_line_buf, something must go wrone', level=ERROR)
+                    self.rx_line_buf.clear()
                 self.rx_line_buf.append(self.rx_buf.rstrip())
                 log('uart rx receive line:', self.rx_buf)
                 self.rx_buf = ''
 
-    def uart_rx_any():
+    def uart_rx_any(self):
         # 返回 rx_line_buf 中剩余的行数
         return len(self.rx_line_buf)
 
@@ -57,7 +63,9 @@ class E104BLE(Device):
         self.uart.write(tx_str)
 
     def uart_tx_raw(self, tx_raw):
+        # 向蓝牙模块写入原始数据
         self.uart.write(tx_raw)
 
     def uart_rx_raw(self):
+        # 从蓝牙模块读取原始数据，绝对用不到，因为有中断
         return self.uart.read()
