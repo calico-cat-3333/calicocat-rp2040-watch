6131abd6257e37f40de90954bba569928ddd3d53
Calico cat <ahsdhmzyn@163.com>
2025-03-19 19:01:44 +0800

    add get_time to log

--- cces/log.py
+++ cces/log.py
@@ -1,6 +1,6 @@
 import sys
 
-from time import ticks_ms
+from time import ticks_ms, ticks_us, ticks_diff
 
 '''
 日志系统
@@ -33,3 +33,13 @@ def log(*args, level=INFO, exc=None, **kwargs):
     if exc == None:
         return
     sys.print_exception(exc)
+
+def get_time(f, *args, **kwargs):
+    myname = f.__name__
+    def new_func(*args, **kwargs):
+        t = ticks_us()
+        result = f(*args, **kwargs)
+        delta = ticks_diff(ticks_us(), t)
+        print('Function {} Time = {:6.3f}ms'.format(myname, delta/1000))
+        return result
+    return new_func
