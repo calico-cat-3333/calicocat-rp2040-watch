cfe8d1ee1bd9aad79537148c78c94a0a8e1a54e1
Calico cat <ahsdhmzyn@163.com>
2025-03-19 11:22:54 +0800

    add start function to task_scheduler

--- cces/task_scheduler.py
+++ cces/task_scheduler.py
@@ -1,15 +1,16 @@
 from _asyncio import TaskQueue
 from _asyncio import Task as _Task
-from time import ticks_ms, ticks_add, ticks_diff
+from time import ticks_ms, ticks_add, ticks_diff, sleep_ms
 from micropython import const
 
 from .log import log
 
-# 简单的任务调度器，参考了 kmk_firmware 项目中使用的任务调度器
-# 利用 _usyncio 中的 TaskQueue 配对堆实现
-# 这些内容原本是为了 uasyncio 模块服务的，所以没有良好的文档记录
-# 系统核心，任务调度有它负责
-# lvgl 事件相关的调度由 lvgl 的 event_loop 调度器负责，那个调度器使用中断完成调度，与这里不冲突。
+'''
+简单的任务调度器，参考了 kmk_firmware 项目中使用的任务调度器
+利用 _usyncio 中的 TaskQueue 配对堆实现
+这些内容原本是为了 uasyncio 模块服务的，所以没有良好的文档记录
+系统核心，任务调度有它负责
+'''
 
 # 任务队列
 _task_queue = TaskQueue()
@@ -68,11 +69,20 @@ class Task:
             log('function', self.func.__name__, 'timeuse:', tu, 'then exit')
             self.enabled = False
 
-# 获取可以开始运行的任务
 def get_due_task():
+    # 获取可以开始运行的任务
     t = _task_queue.peek()
     if t != None and ticks_diff(ticks_ms(), t.ph_key) >= 0:
         _task_queue.pop()
         return t.coro
     else:
         return None
+
+def start():
+    # 启动调度器，并完全接管系统
+    while True:
+        task = get_due_task()
+        if task != None:
+            task()
+        else:
+            sleep_ms(1)
