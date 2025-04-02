a92ad83cd8aa8437c6682b2a628e7faa6c1096d5
Calico cat <ahsdhmzyn@163.com>
2025-04-02 18:26:01 +0800

    finish gpio key driver

--- cces/drvs/gpio_button.py
+++ cces/drvs/gpio_button.py
@@ -1,7 +1,38 @@
+import lvgl as lv
 from machine import Pin
 
-class GPIOButton:
-    def __init__(self, pin, irq_type):
-        self.pin = pin
-        self.pin.irq()
+from . import Device_lv
+from .. import powermanager
+import micropython
 
+# 这不是标准的 lvgl 输入设备，但是他需要使用 lvgl 输入设备的 api 以实现包括检测系统状态等功能
+class GPIOButton(Device_lv):
+    def __init__(self, pin):
+        self.pin = pin
+        self.pin.irq(handler=self.int_pin_read, trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING)
+        self.pressed = False
+
+    def int_pin_read(self, pin):
+        if pin.value():
+            self.pressed = False
+        else:
+            self.pressed = True
+
+    def after_lvgl_init(self):
+        self.indev_drv = lv.indev_create()
+        self.indev_drv.set_type(lv.INDEV_TYPE.KEYPAD)
+        self.indev_drv.set_read_cb(self.button_read_cb)
+        self.group = lv.group_create()
+        self.group.set_default()
+        self.indev_drv.set_group(self.group)
+
+    def button_read_cb(self, indev_drv, data):
+        if self.pressed:
+            if powermanager.sys_sleeping:
+                powermanager.try_wakeup() # 如果是唤醒，则不进行任何操作
+                data.state = lv.INDEV_STATE.RELEASED
+            else:
+                data.state = lv.INDEV_STATE.PRESSED
+        else:
+            data.state = lv.INDEV_STATE.RELEASED
+        data.key = lv.KEY.ESC
