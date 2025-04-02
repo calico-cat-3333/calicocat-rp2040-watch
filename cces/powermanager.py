d768cd780dd3aece09b131bb923acabd1abc7ce6
Calico cat <ahsdhmzyn@163.com>
2025-04-02 18:25:40 +0800

    update powermanager

--- cces/powermanager.py
+++ cces/powermanager.py
@@ -1,5 +1,6 @@
 import time
 import machine
+import lvgl as lv
 from micropython import const, schedule
 
 from . import hal, settingsdb
@@ -10,7 +11,7 @@ sys_sleeping = False
 allow_sleep = True
 _SLEEP_CHECK_PERIOD = const(1000)
 
-def check_sleep(_):
+def check_sleep():
     global sys_sleeping
     timeout = settingsdb.get('screenoff_timeout', 30)
     if allow_sleep and hal.dispdev.disp_drv.get_inactive_time() // 1000 > timeout:
@@ -23,10 +24,6 @@ def check_sleep(_):
             log('system wakeup')
             sys_sleeping = False
             hal.on_wakeup()
-
-def schedule_check_sleep():
-    # try_wakeup 可能在中断服务函数中执行，为了保证所有睡眠和唤醒操作的原子性，这里需要使用这种方法实现。
-    schedule(check_sleep, 0)
 
 def try_wakeup():
     # 程序中尝试主动唤醒显示器
@@ -47,5 +44,5 @@ def prevent_sleep(v=None):
 
 def start():
     global sleep_check_task
-    sleep_check_task = Task(schedule_check_sleep, _SLEEP_CHECK_PERIOD)
+    sleep_check_task = Task(check_sleep, _SLEEP_CHECK_PERIOD)
     sleep_check_task.start()
