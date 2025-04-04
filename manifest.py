f9e40446215f0f8102c9d6989c80d54e102e9f3f
Calico cat <ahsdhmzyn@163.com>
2025-04-04 22:12:30 +0800

    add howto use to manifest.py

--- manifest.py
+++ manifest.py
@@ -1,3 +1,6 @@
+# 使用方法：
+# 在 lv_micropython/ports/rp2/boards/manifest.py 中添加：
+# include('path/to/this/manifest.py')
 package('cces_drvs')
 module('board.py', base_path='lib')
 module('extboard.py', base_path='lib')
