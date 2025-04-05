d2a2d5cfd63ea53745109e567c1c043f9bb6bb26
Calico cat <ahsdhmzyn@163.com>
2025-04-05 21:18:36 +0800

    从 debug helper 中分离 simuctrl

--- apps/simu_ctrl_app.py
+++ apps/simu_ctrl_app.py
@@ -9,24 +9,18 @@ from cces.log import log
 
 class MainActivity(Activity):
     def __init__(self):
-        self.fl = [('notify send', self.send_notify),
-                   ('notify remove', self.remove_notify),
-                   ('info', self.info_example),
-                   ('askyesno', self.askyesno_example),
-                   ('input number', self.input_number_example),
-                   ('slider', self.slider_example),
-                   ('GB Info', self.gbinfo),
-                   ]
-        self.nid = None
-        fll = ['Simuctrl',
-               ('bat val', self.set_dummy_bat_value),
-               ('set step', self.set_dummy_setp_value),
-               ('tog ble', self.dummyble_tog),
-               ('GB weather', self.send_dummy_weather),
-               ('GB Music', self.send_dummy_music),
-               ]
-        if sys.platform == 'linux':
-            self.fl.extend(fll)
+        if sys.platform == 'rp2':
+            log('should not load this app on real machine')
+            self.fl = [('No use on', self.exit_btn_cb),
+                       ('real machine', self.exit_btn_cb)
+                       ]
+        else:
+            self.fl = [('bat val', self.set_dummy_bat_value),
+                    ('set step', self.set_dummy_setp_value),
+                    ('tog ble', self.dummyble_tog),
+                    ('GB weather', self.send_dummy_weather),
+                    ('GB Music', self.send_dummy_music),
+                    ]
 
     def setup(self):
         self.scr.add_event_cb(self.gesture_event_cb, lv.EVENT.GESTURE, None)
@@ -34,14 +28,11 @@ class MainActivity(Activity):
         self.dlst.set_size(200, 240)
         self.dlst.align(lv.ALIGN.CENTER, 20, 0)
         self.dlst.set_style_border_side(lv.BORDER_SIDE.NONE, lv.PART.MAIN | lv.STATE.DEFAULT)
-        title = self.dlst.add_text('Tester')
+        title = self.dlst.add_text('SimuCtrl')
         title.set_style_pad_bottom(10, lv.PART.MAIN | lv.STATE.DEFAULT)
         title.set_style_pad_top(20, lv.PART.MAIN | lv.STATE.DEFAULT)
 
         for f in self.fl:
-            if isinstance(f, str):
-                self.dlst.add_text(f)
-                continue
             btn = self.dlst.add_button(lv.SYMBOL.FILE, f[0])
             btn.set_flex_align(lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.SPACE_EVENLY)
             btn.add_event_cb(f[1], lv.EVENT.CLICKED, None)
@@ -70,42 +61,6 @@ class MainActivity(Activity):
         if gesture == lv.DIR.RIGHT:
              self.exit(lv.SCR_LOAD_ANIM.OVER_RIGHT)
 
-    def send_notify(self, _):
-        self.nid = notification.send('example notify', 'example notify text.')
-
-    def remove_notify(self, _):
-        if not self.nid:
-            InfoActivity('Error', 'You Need Send notify before remove it').launch()
-        notification.remove(self.nid)
-
-    def info_example(self, _):
-        def ok_click():
-            log('ok clicked!')
-        InfoActivity('title', 'text', ok_click).launch()
-
-    def askyesno_example(self, _):
-        def yes_click():
-            log('yes clicked!')
-        def no_click():
-            log('no clicked!')
-        AskYesNoActivity('title', 'text', yes_click, no_click).launch()
-
-    def input_number_example(self, _):
-        def getnumb(numb):
-            log(numb)
-        NumberInputActivity('title', -10, 10, getnumb, 5).launch()
-
-    def slider_example(self, _):
-        def ok_cb(numb):
-            log('ok', numb)
-        def vc_cb(n):
-            log('value change', n)
-        def sr_cb(n):
-            log('slider released', n)
-        def no_cb(n):
-            log('no', n)
-        SliderActivity('title', 0, 100, 0, '%', ok_cb, no_cb, sr_cb, vc_cb, 5).launch()
-
     def gbinfo(self, _):
         gadgetbridge.send_msg('info', 'test info')
 
@@ -131,4 +86,4 @@ class MainActivity(Activity):
             log('set fake step value:', v)
         NumberInputActivity('Dummy step', 0, 30000, setv, 33).launch()
 
-appmeta = AppMeta('测试器', lv.SYMBOL.FILE, MainActivity)
+appmeta = AppMeta('SimuCtrl', lv.SYMBOL.FILE, MainActivity)
