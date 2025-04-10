da48b24cb721e4211c3f718915becef52a106ed1
Calico cat <ahsdhmzyn@163.com>
2025-04-10 09:58:58 +0800

    forzen cces

--- manifest.py
+++ manifest.py
@@ -1,8 +1,9 @@
 # 使用方法：
 # 在 lv_micropython/ports/rp2/boards/manifest.py 中添加：
 # include('path/to/this/manifest.py')
-package('cces_drvs')
+package('cces_drv')
 module('board.py', base_path='lib')
 module('extboard.py', base_path='lib')
 module('pcf8574.py', base_path='lib')
 module('cst816s.py', base_path='lib')
+package('cces')
